d2b7b6f7ff4eb7948ffc8db708d4631f4955eb3f
Prabhjot Kaur <58880607+Prabhjot410@users.noreply.github.com>
2022-02-02 23:55:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Amount = ( bill * tip ) / ppl
+//total = ((amount * ppl) + Bill ) /ppl
+
+const bill = document.getElementById('bill');
+const ppl = document.getElementById('ppl');
+const amount = document.getElementById('amount');
+const total = document.getElementById('total');
+const reset = document.getElementById('reset');
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const btn3 = document.getElementById('btn3');
+const btn4 = document.getElementById('btn4');
+const btn5 = document.getElementById('btn5');
+const btn6 = document.getElementById('btn6');
+
+
+
